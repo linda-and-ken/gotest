19aeb13bf4c489bc4eb4f1bcc5e701c0e44f0ea7
linlin <>
2026-04-07 21:30:13 +0800

    feat:cc

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello world")
+		c.String(http.StatusOK, "hello world xxxx")
 	})
 
 	addr := ":8080"
