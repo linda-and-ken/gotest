48cef9a1aa4c6629a907e64709d892a4430a3b3b
linlin <>
2026-04-28 23:00:44 +0800

    feat:多url监听

--- main.go
+++ main.go
@@ -9,8 +9,18 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello , i am linda \n")
+	r.GET("/*path", func(c *gin.Context) {
+		path := c.Param("path")
+		if path == "" {
+			path = "/"
+		}
+
+		if path == "/aa/bbb" {
+			c.String(http.StatusOK, "%s", path)
+			return
+		}
+
+		c.String(http.StatusOK, "hello , i am linda %s", path)
 	})
 
 	addr := ":8080"
